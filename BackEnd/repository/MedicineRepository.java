9ef3ee8712c9295b9ea46ad6c9211d2b184c2b1f
Vedant Kamble <vedantkamble9921@gmail.com>
2026-06-13 14:18:49 +0530

    Delete BackEnd directory

--- BackEnd/repository/MedicineRepository.java
+++
@@ -1,7 +0,0 @@
-package com.medstore.backend.repository;
-
-import com.medstore.backend.entity.Medicine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicineRepository extends JpaRepository<Medicine, Long> {
-}
